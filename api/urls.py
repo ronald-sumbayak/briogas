bf2ee2d6ab8110f4759d04aaf62c4e2d18db948f
ronald-sumbayak <ronaldsumbayak611@gmail.com>
2017-08-11 10:35:30 +0700

    finalize update data

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,6 @@ from api import views
 urlpatterns = [
     url (r'^token', obtain_auth_token),
     url (r'^data/retrieve', views.retrieve_data),
-    url (r'^data/update', views.update_data),
+    url (r'^data/update', views.UpdateData.as_view ()),
     url (r'^toggle-katup', views.toggle_katup)
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -11,13 +12,14 @@ def retrieve_data (request):
     serializer = serializers.DataSerializer (data)
     return Response (serializer.data)
 
-@api_view (['POST', 'PATCH'])
-@permission_classes ([IsAuthenticated])
-def update_data (request):
-    serializer = serializers.UpdateDataSerializer (data = request.data)
-    serializer.is_valid (raise_exception = True)
-    serializer.save ()
-    return Response (serializer.data)
+class UpdateData (generics.UpdateAPIView):
+    serializer_class = serializers.DataSerializer
+    queryset = models.Data.objects.all ()
+    
+    def get_object (self):
+        if self.request.data.get ('id'):
+            return self.queryset.get (id = self.request.data['id'])
+        raise ValidationError ("id must be provided", "required")
 
 @api_view (['POST', 'PATCH'])
 @permission_classes ([IsAuthenticated])
