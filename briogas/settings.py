6c085b6c47669799e9e34d449ba944206876927a
ronald-sumbayak <ronaldsumbayak611@gmail.com>
2017-08-12 21:13:55 +0700

    allow any connection

--- briogas/settings.py
+++ briogas/settings.py
@@ -126,6 +126,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated']
-    # 'DEFAULT_PERMISSION_CLASSES': 'rest_framework.permissions.AllowAny'
+    # 'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated']
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny']
 }
